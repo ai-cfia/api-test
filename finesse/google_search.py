7e5557bbb424c069b2e3b1e15230c71112f46a3f
Ibrahim Kabir <ibrahimkabir2003@gmail.com>
2024-03-22 11:39:55 -0400

    issue #6: bing search by scrapping

--- finesse/google_search.py
+++
@@ -1,7 +0,0 @@
-from googlesearch import search
-
-def get_google_search_urls(query: str, num_results: int = 100) -> list[str]:
-    links = []
-    for url in search(query, num_results, sleep_interval=1):
-        links.append(url)
-    return links
--- tests/test_bing_search.py
+++ tests/test_bing_search.py
@@ -1,11 +1,11 @@
 import unittest
-from finesse.google_search import get_google_search_urls
+from finesse.bing_search import get_bing_search_urls
 
-class TestGoogleSearch(unittest.TestCase):
+class TestBingSearch(unittest.TestCase):
     def test_get_google_search_urls(self):
         query = "Canada Food Inspection Agency"
         num_results = 10
-        urls = get_google_search_urls(query, num_results)
+        urls = get_bing_search_urls(query, num_results)
         self.assertEqual(len(urls), num_results)
         self.assertTrue(all(url.startswith("http") for url in urls))
 
